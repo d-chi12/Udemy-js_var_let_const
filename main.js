249e759de7972458da0eedb28324bac90d535563
USER <user@USERnoMacBook-ea.local>
2020-04-18 19:11:54 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,19 @@
+// let(上書き可能), const(上書き不可), var
+
+// const name = 'tom';
+let name = 'tom'
+name = 'tim'
+console.log('hello' + name);
+
+
+// Number, String, Boolean, Undefinsd, Null, Symbol
+
+// 動的型付け言語
+let variable = 'str';
+variable = 12;
+variable = false;
+variable = undefined;
+
+console.log(typeof variable);
+
+
